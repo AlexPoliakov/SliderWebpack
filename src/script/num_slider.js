28fb161d65c2b43d09fec594d3a6b202e98c8b15
Aleksandr Poliakov <alpoliakov@ukr.net>
2017-09-01 15:22:57 +0300

    Some choose in connecting scripts

--- src/script/num_slider.js
+++ src/script/num_slider.js
@@ -1,45 +1,46 @@
 'use strict';
 
 class NumericInput {
-    constructor(options) {
-        this.elem = options.elem;
-        this.slider = document.querySelector(`.slider`);
-        this.thumb = document.querySelector(`.slider__thumb`);
+  constructor(options) {
+    this.elem = options.elem;
+    this.slider = document.querySelector(`.slider`);
+    this.thumb = document.querySelector(`.slider__thumb`);
 
-        this.checkValue = this.checkValue.bind(this);
-        this.inputNum = this.inputNum.bind(this);
-        this.setThumb = this.setThumb.bind(this);
-        this.elem.addEventListener(`keypress`, this.inputNum);
+    this.checkValue = this.checkValue.bind(this);
+    this.inputNum = this.inputNum.bind(this);
+    this.setThumb = this.setThumb.bind(this);
+    this.elem.addEventListener(`keypress`, this.inputNum);
+  }
+
+  inputNum(event) {
+    this.target = event.target;
+
+    if (!isFinite(String.fromCharCode(event.keyCode))) {
+      event.preventDefault();
+    } else {
+      this.elem.addEventListener(`input`, this.checkValue);
     }
+  }
 
-    inputNum(event) {
-        this.target = event.target;
+  checkValue() {
+    if (this.elem.value > 100) this.elem.value = 100;
+    this.setThumb(this.elem.value);
+  }
 
-        if(!isFinite(String.fromCharCode(event.keyCode))) {
-            event.preventDefault();
-        }else {
-            this.elem.addEventListener(`input`, this.checkValue);
-        }
-    }
+  setThumb(num) {
+    this.length =
+      num * this.slider.offsetWidth / 100 - this.thumb.offsetWidth / 2;
+    if (this.length < 0) this.length = 0;
 
-    checkValue() {
-       if (this.elem.value > 100) this.elem.value = 100;
-       this.setThumb(this.elem.value);
-    }
+    this.eventInput = new CustomEvent(`setlength`, {
+      bubbles: true,
+      detail: {
+        length: `${this.length}px`,
+      },
+    });
 
-    setThumb(num) {
-        this.length = (num   * this.slider.offsetWidth) / 100 - this.thumb.offsetWidth / 2;
-        if (this.length < 0) this.length = 0;
-
-        this.eventInput = new CustomEvent(`setlength`, {
-            bubbles: true,
-            detail: {
-                length: `${this.length}px`
-            }
-        });
-
-        this.elem.dispatchEvent(this.eventInput);
-    }
+    this.elem.dispatchEvent(this.eventInput);
+  }
 }
 
-export {NumericInput};
+export { NumericInput };
--- src/script/slider.js
+++ src/script/slider.js
@@ -1,88 +1,91 @@
 'use strict';
 
 class Slider {
-    constructor(options) {
-        this._elem = options.elem;
-        this._elemCapture = this._elemCapture.bind(this);
-        this._zeroingOut = this._zeroingOut.bind(this);
-        this._moveAt = this._moveAt.bind(this);
-        this._dragstart = this._dragstart.bind(this);
-        this._elem.addEventListener(`mousedown`, this._elemCapture);
-        this.setValue = this.setValue.bind(this);
+  constructor(options) {
+    this._elem = options.elem;
+    this._elemCapture = this._elemCapture.bind(this);
+    this._zeroingOut = this._zeroingOut.bind(this);
+    this._moveAt = this._moveAt.bind(this);
+    this._dragstart = this._dragstart.bind(this);
+    this._elem.addEventListener(`mousedown`, this._elemCapture);
+    this.setValue = this.setValue.bind(this);
 
-        this._thumb = this._elem.querySelector(`.slider__thumb`);
-        this._slider = this._elem.querySelector(`.slider`);
-        this._html = document.documentElement;
+    this._thumb = this._elem.querySelector(`.slider__thumb`);
+    this._slider = this._elem.querySelector(`.slider`);
+    this._html = document.documentElement;
+  }
+
+  _elemCapture(event) {
+    this._target = event.target;
+    this._event = event;
+
+    this._target.addEventListener(`dragstart`, this._dragstart);
+
+    if (!this._target.classList.contains(`slider__thumb`)) return;
+
+    this._slider.addEventListener(`mousemove`, this._moveAt);
+    this._html.addEventListener(`mouseup`, this._zeroingOut);
+
+    this._coordsThumb = this._getCoords(this._target);
+    this._shiftX = this._event.pageX - this._coordsThumb.left;
+  }
+
+  _moveAt(event) {
+    this._event = event;
+    this._target = event.target;
+
+    if (!this._target.classList.contains(`slider`)) return;
+
+    this._sliderCoords = this._getCoords(this._slider);
+    this._leftThumb =
+      this._event.pageX - this._sliderCoords.left - this._shiftX;
+
+    if (this._leftThumb < 0) this._leftThumb = 0;
+
+    this._rightThumb = this._slider.offsetWidth - this._thumb.offsetWidth;
+
+    if (this._leftThumb > this._rightThumb) {
+      this._leftThumb = this._rightThumb;
     }
 
-    _elemCapture(event) {
-        this._target = event.target;
-        this._event = event;
+    this.setValue(this._leftThumb);
+  }
 
-        this._target.addEventListener(`dragstart`, this._dragstart);
+  setValue(value) {
+    this._thumb.style.left = `${value}px`;
+    this.numeric = Math.round(
+      value / (this._slider.offsetWidth - this._thumb.offsetWidth) * 100,
+    );
 
-        if(!this._target.classList.contains(`slider__thumb`)) return;
+    this.valueSlider = new CustomEvent(`setting`, {
+      bubbles: true,
+      detail: {
+        value: this.numeric,
+      },
+    });
 
-        this._slider.addEventListener(`mousemove`, this._moveAt);
-        this._html.addEventListener(`mouseup`, this._zeroingOut);
+    this._slider.dispatchEvent(this.valueSlider);
+  }
 
-        this._coordsThumb = this._getCoords(this._target);
-        this._shiftX = this._event.pageX - this._coordsThumb.left;
-    }
+  _zeroingOut() {
+    this._slider.removeEventListener(`mousemove`, this._moveAt);
+    this._html.removeEventListener(`mouseup`, this._zeroingOut);
+  }
 
-    _moveAt(event) {
-        this._event = event;
-        this._target = event.target;
+  _dragstart() {
+    event.preventDefault();
+  }
 
-        if(!this._target.classList.contains(`slider`)) return;
+  _getCoords(elem) {
+    let box = elem.getBoundingClientRect();
 
-        this._sliderCoords = this._getCoords(this._slider);
-        this._leftThumb = this._event.pageX - this._sliderCoords.left - this._shiftX;
-
-        if (this._leftThumb < 0) this._leftThumb = 0;
-
-        this._rightThumb = this._slider.offsetWidth - this._thumb.offsetWidth;
-
-        if (this._leftThumb > this._rightThumb) {
-            this._leftThumb = this._rightThumb;
-        }
-
-        this.setValue(this._leftThumb);
-    }
-
-    setValue(value) {
-        this._thumb.style.left = `${value}px`;
-        this.numeric = Math.round(value  / (this._slider.offsetWidth - this._thumb.offsetWidth) * 100);
-
-        this.valueSlider = new CustomEvent(`setting`, {
-            bubbles: true,
-            detail: {
-                value: this.numeric,
-            }
-        });
-
-        this._slider.dispatchEvent(this.valueSlider);
-    }
-
-    _zeroingOut() {
-        this._slider.removeEventListener(`mousemove`, this._moveAt);
-        this._html.removeEventListener(`mouseup`, this._zeroingOut);
-    }
-
-    _dragstart() {
-        event.preventDefault();
-    }
-
-    _getCoords(elem) {
-        let box = elem.getBoundingClientRect();
-
-        return {
-            top: box.top + pageYOffset,
-            bottom: box.bottom + pageYOffset,
-            left: box.left + pageXOffset,
-            right: box.right + pageXOffset
-        };
-    }
+    return {
+      top: box.top + pageYOffset,
+      bottom: box.bottom + pageYOffset,
+      left: box.left + pageXOffset,
+      right: box.right + pageXOffset,
+    };
+  }
 }
 
-export {Slider};
+export { Slider };
